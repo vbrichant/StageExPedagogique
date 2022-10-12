e9ed41479ab22d1235a485b65cbe38b29ed27bcd
V.Brichant <he201583@students.ephec.be>
2022-10-12 17:24:04 +0200

    Modification du modèle SessionFormation pour avoir un champ date et un champ time => modifiaction dans l'app pour prendre en compte les changement
    Modification de NewSessionForm pour afficher n'afficher que les session crée par le current user (pas encore fonctionnel)

--- formation/forms.py
+++ formation/forms.py
@@ -1,11 +1,20 @@
+import contextvars
+
+import self
 from django import forms
 from django.utils import timezone
 from django.shortcuts import get_object_or_404
 
-from .models import Formateur, Formation, SessionFormation, Inscription
+from .models import Formateur, Formation, SessionFormation, Inscription, User
 
 
-def get_formateur_name():
+def get_current_user():
+    pass
+    #request
+    #return request.user
+
+
+def get_formateur_name_list():
     formateur_list = []
     for formateur in Formateur.objects.all():
         formateur_list.append(formateur.get_name)
@@ -15,8 +24,10 @@ def get_formateur_name():
 def get_formation_name_list():
     formation_list = Formation.objects.all()
     formation_list_name = []
+    print("")
     for formation in formation_list:
-        formation_list_name.append((formation.id, formation.name))
+        if formation.formateur.user == "self.user":
+            formation_list_name.append((formation.id, formation.name))
     return formation_list_name
 
 
@@ -39,21 +50,28 @@ class NewFormationForm(forms.Form):
         new_formation.save()
 
 
-class NewSessionForm(forms.Form):
-    print()
-    get_formation_name_list()
-    formation_name = forms.ChoiceField(choices=get_formation_name_list())
-    formation_place = forms.CharField(max_length=50)
-    formation_date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'],
-                                         initial=timezone.now())
-    formation_max_students = forms.IntegerField(widget=forms.NumberInput, min_value=5, max_value=100)
+class NewSessionForm(forms.ModelForm):
+    class Meta:
+        model = SessionFormation
+        fields = ['formation', 'place', 'date', 'time', 'max_students']
+        widgets = {
+            'formation': forms.ModelChoiceField(queryset=Formation.objects.filter(formateur__user=get_current_user()),
+                                                to_field_name='name'),
+            'date': forms.SelectDateWidget(),
+            'time': forms.TimeInput(attrs={'type': 'time'})
+        }
 
     def create_new_session(self):
         data = self.cleaned_data
-        new_session = SessionFormation(formation_id=data["formation_name"], place=data["formation_place"],
-                                       date=data["formation_date"],
-                                       max_students=data["formation_max_students"])
+        new_session = SessionFormation(formation_id=data["formation"].id, place=data["place"],
+                                       date=data["date"], time=data["time"],
+                                       max_students=data["max_students"])
         new_session.save()
+
+    # formation_name = forms.ChoiceField(choices=get_formation_name_list)
+    # session_place = forms.CharField(max_length=50)
+    # session_date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'],initial=timezone.now())
+    # formation_max_students = forms.IntegerField(widget=forms.NumberInput, min_value=5, max_value=100)
 
 
 class NewRegistrationForm(forms.Form):
--- formation/models.py
+++ formation/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
@@ -37,25 +39,32 @@ class Formation(models.Model):
 
 class SessionFormation(models.Model):
     formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
-    date = models.DateTimeField("formation date")
+    date = models.DateField("date")
+    time = models.TimeField("time")
     place = models.CharField(max_length=200)
     max_students = models.IntegerField()
 
     def __str__(self):
-        return self.formation.name + " " + str(self.date)
+        return self.formation.name + " " + str(self.get_date_time())
 
     def get_count_registration(self):
         return self.inscription_set.count()
 
     def get_students_registered(self):
+        print("ici")
         inscription_list = self.inscription_set.values_list("student", flat=True)
         student_list = []
         for inscription in inscription_list:
             student_list.append(int(inscription))
+        print(student_list)
+        print(inscription_list)
         return inscription_list
 
+    def get_date_time(self):
+        return datetime.combine(date=self.date, time=self.time)
+
     def is_open(self):
-        now = timezone.now()
+        now = datetime.today().date()
         return self.date > now
 
 
