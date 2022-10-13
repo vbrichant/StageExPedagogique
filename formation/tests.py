fe4deafe2da9cf432316a0103341e21f6d7be04c
V.Brichant <he201583@students.ephec.be>
2022-10-13 17:06:58 +0200

    Test des view : - FormationListView - FormationListForFormateurView - RegistrationListForStudentsViewTest

--- formation/tests.py
+++ formation/tests.py
@@ -9,8 +9,8 @@ def create_user(user_name):
     return User.objects.create_user(username=user_name)
 
 
-def create_student(student_name):
-    return Student.objects.create(user=create_user(user_name=student_name))
+def create_student(student_name, matricule):
+    return Student.objects.create(user=create_user(user_name=student_name), matricule=matricule)
 
 
 def create_formateur(formateur_name):
@@ -28,6 +28,9 @@ def create_session_formation(formation: Formation, date: datetime, place: str, m
                                            place=place,
                                            max_students=max_students)
 
+
+def create_registration(session: SessionFormation, student: Student):
+    return Inscription.objects.create(session=session, student=student)
     # Create your tests here.
 
 
@@ -54,7 +57,7 @@ class SessionFormationModelTest(TestCase):
         self.assertIs(future_session.is_open(), False)
 
 
-class FormationListForFormateurViewTest(TestCase):
+class FormationListViewTest(TestCase):
 
     def test_without_formation(self):
         response = self.client.get(reverse('formation:formation_list'))
@@ -83,24 +86,75 @@ class FormationListForFormateurViewTest(TestCase):
         )
 
 
-class FormationListForFormateurView(TestCase):
+class FormationListForFormateurViewTest(TestCase):
+
     def test_without_formation(self):
-        formateur1 = create_formateur(formateur_name="formateur_test")
-        url = reverse('formation:formation_list_current_formateur', args=[formateur1.user.formateur.id])
+        formateur1 = create_formateur(formateur_name="formateur_test1")
+        self.client.force_login(User.objects.get_or_create(username='formateur_test1')[0])
+        url = reverse('formation:formation_list_current_formateur', args=[formateur1.id])
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 302)
-        # self.assertContains(response, "Aucune formation n'est disponible")
-        # self.assertQuerysetEqual(response.context['formation_list'], [])
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Aucune formation n'est disponible")
+        self.assertQuerysetEqual(response.context['formation_list'], [])
 
     def test_with_one_formation_created_by_formateur(self):
-        pass
+        formateur1 = create_formateur(formateur_name="formateur_test1")
+        formation1 = create_formation(formateur=formateur1, name="formation_test_1")
+        self.client.force_login(User.objects.get_or_create(username='formateur_test1')[0])
+        url = reverse('formation:formation_list_current_formateur', args=[formateur1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, formation1.name)
+        self.assertQuerysetEqual(response.context['formation_list'], [formation1], ordered=False)
 
     def test_with_one_formation_not_created_by_formateur(self):
-        pass
+        formateur1 = create_formateur(formateur_name="formateur_test1")
+        formateur2 = create_formateur(formateur_name="formateur_test2")
+        formation1 = create_formation(formateur=formateur2, name="formation_test_1")
+        self.client.force_login(User.objects.get_or_create(username='formateur_test1')[0])
+        url = reverse('formation:formation_list_current_formateur', args=[formateur1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Aucune formation n'est disponible")
+        self.assertQuerysetEqual(response.context['formation_list'], [], ordered=False)
 
     def test_with_two_formation_but_one_created_by_formateur(self):
-        pass
+        formateur1 = create_formateur(formateur_name="formateur_test1")
+        formateur2 = create_formateur(formateur_name="formateur_test2")
+        formation1 = create_formation(formateur=formateur1, name="formation_test_1")
+        formation2 = create_formation(formateur=formateur2, name="formation_test_2")
+        self.client.force_login(User.objects.get_or_create(username='formateur_test1')[0])
+        url = reverse('formation:formation_list_current_formateur', args=[formateur1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, formation1.name)
+        self.assertQuerysetEqual(response.context['formation_list'], [formation1], ordered=False)
 
 
-class RegistrationListForStudentsView(TestCase):
-    pass
+class RegistrationListForStudentsViewTest(TestCase):
+    def test_without_registration(self):
+        date = datetime.now()
+        student1 = create_student(student_name="student_test_1",matricule='HE200000')
+        formateur1 = create_formateur(formateur_name="formateur_test_1")
+        formation1 = create_formation(formateur=formateur1, name="formation_test_1")
+        session1 = create_session_formation(formation=formation1, date=date, place='test', max_students=15)
+        self.client.force_login(User.objects.get_or_create(username='student_test_1')[0])
+        url = reverse('formation:inscription_list_current_student', args=[student1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Aucune inscription disponible")
+        self.assertQuerysetEqual(response.context['registration_list'], [])
+
+    def test_with_one_registration_for_student(self):
+        date = datetime.now()
+        student1 = create_student(student_name="student_test_1",matricule="HE200000")
+        formateur1 = create_formateur(formateur_name="formateur_test_1")
+        formation1 = create_formation(formateur=formateur1, name="formation_test_1")
+        session1 = create_session_formation(formation=formation1, date=date, place='test', max_students=15)
+        registration1 = create_registration(session=session1, student=student1)
+        self.client.force_login(User.objects.get_or_create(username='student_test_1')[0])
+        url = reverse('formation:inscription_list_current_student', args=[student1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, registration1.session.formation)
+        self.assertQuerysetEqual(response.context['registration_list'], [registration1])
