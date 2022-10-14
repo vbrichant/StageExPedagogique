25480bac36fb3a0d1179c03d130aff53bc46629a
V.Brichant <he201583@students.ephec.be>
2022-10-14 14:11:00 +0200

    Refactor de code : Modification du formulaire new_formation_form Utilisation de ModelForm et de CreateView

--- formation/forms/new_formation_form.py
+++ formation/forms/new_formation_form.py
@@ -1,14 +1,10 @@
 from django import forms
+from django.forms import ModelForm
 
 from formation.model.Formation import Formation
 
 
-class NewFormationForm(forms.Form):
-    formation_name = forms.CharField()
-    formation_description = forms.CharField(widget=forms.Textarea)
-
-    def create_new_formation(self, user_logged):
-        data = self.cleaned_data
-        new_formation = Formation(name=data["formation_name"], description=data["formation_description"],
-                                  formateur=user_logged)
-        new_formation.save()
+class NewFormationForm(ModelForm):
+    class Meta:
+        model = Formation
+        fields = ('name', 'description')
